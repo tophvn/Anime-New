a6670443727cb5d385b0fdb8f48c10a8423f9b4e
HoangHai-Toph <113342289+tophvn@users.noreply.github.com>
2024-04-25 07:17:56 +0700

    Update slide.js

--- js/slide.js
+++ js/slide.js
@@ -6,38 +6,27 @@ const intervalTime = 5000;
 let slideInterval;
 
 const nextSlide = () => {
-    // Get current class
     const current = document.querySelector('.current');
-    // remove current class
     current.classList.remove('current');
-    // Check for next slide
     if (current.nextElementSibling) {
-        // Add current to next sibling
         current.nextElementSibling.classList.add('current');
     } else {
-        // Add current to start
         slides[0].classList.add('current');
     }
     setTimeout(() => current.classList.remove('current'));
 };
 
 const prevSlide = () => {
-    // Get current class
     const current = document.querySelector('.current');
-    // remove current class
     current.classList.remove('current');
-    // Check for prev slide
     if (current.previousElementSibling) {
-        // Add current to next sibling
         current.previousElementSibling.classList.add('current');
     } else {
-        // Add current to last
         slides[slides.length - 1].classList.add('current');
     }
     setTimeout(() => current.classList.remove('current'));
 };
 
-// Button events
 next.addEventListener('click', e => {
     nextSlide();
     if (auto) {
@@ -57,8 +46,6 @@ prev.addEventListener('click', e => {
 
 });
 
-// Auto slide
 if (auto) {
-    // Run next slide at interval time
     slideInterval = setInterval(nextSlide, intervalTime);
 }
